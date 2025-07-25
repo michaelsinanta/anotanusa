cbd3240590b3398f5113da522c9edb18874873f1
michaelsinanta <michaelchristlambert@gmail.com>
2025-07-25 14:59:35 +0700

    fix: build error

--- src/lib/actions/creator.ts
+++ src/lib/actions/creator.ts
@@ -48,7 +48,8 @@ export async function createProject(
 
     const csvText = await file.text();
     const parsed = Papa.parse(csvText, { header: false });
-    const processedData = parsed.data.slice(1).map((columns: string[]) => {
+    const processedData = parsed.data.slice(1).map((value) => {
+      const columns = value as string[];
       const text = columns[0] || "";
       const choices = columns.slice(1).filter((choice) => choice !== "");
       return { text, choices };
